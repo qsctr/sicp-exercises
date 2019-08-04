bc87acfaa286e89d4ce16a7de1d78becf10a986f
Bretton <qsctr@users.noreply.github.com>
2019-08-03 22:59:14 -0700

    Add 2.50-2.51

--- 2.scm
+++ 2.scm
@@ -812,3 +812,43 @@ k-th column."
           (make-vect 0 0.5))))
 
 ; TODO: wave
+
+; 50
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (rotate270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+; 51
+
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-bottom (transform-painter painter1
+                                           (make-vect 0.0 0.0)
+                                           (make-vect 1.0 0.0)
+                                           split-point))
+          (paint-top (transform-painter painter2
+                                        split-point
+                                        (make-vect 1.0 0.5)
+                                        (make-vect 0.0 1.0))))
+      (lambda (frame)
+        (paint-top frame)
+        (paint-bottom frame)))))
+
+(define (below-alt painter1 painter2)
+  (rotate270 (beside (rotate90 painter2)
+                     (rotate90 painter1))))
