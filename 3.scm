ad79782c6d37924e5fd35a7f0209642a8a15e888
Bretton <qsctr@users.noreply.github.com>
2019-12-16 09:52:31 +0800

    Add auxiliary definitions for or-gate-compound

--- 3.scm
+++ 3.scm
@@ -976,6 +976,27 @@ memo-fib, so the results cannot be saved in the table."
 
 ;;; 29
 
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda () (set-signal! output new-value)))))
+  (add-action! input invert-input) 'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda () (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
 (define (or-gate-compound a1 a2 output)
   (let ((w1 (make-wire)) (w2 (make-wire)) (w3 (make-wire)))
     (inverter a1 w1)
